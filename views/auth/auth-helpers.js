32c332e5f4fb11df15a6c0d7e4a41feb80d58b33
Jerel Crespo <mrc220@live.com>
2017-01-29 17:01:06 -0500

    add auth routes

--- views/auth/auth-helpers.js
+++ views/auth/auth-helpers.js
@@ -1,7 +1,33 @@
 const bcrypt = require('bcryptjs');
 const models = require('../db/models/index');
 
+// function to compare the input password compared to db
 function comparePass(userPassword, databasePassword) {
   return bcrypt.compareSync(userPassword, databasePassword);
 }
 
+// user redirected if already logged in
+function loginRedirect(req, res, next) {
+  if (req.user) returnres.status(401).json(
+    { status: 'You are already logged in!' }
+  );
+
+  return next();
+}
+
+// create user in db
+function createUser(req, res) {
+  const salt = bcrypt.genSaltSync();
+  const hash = bcrypt.hashSync(req.body.password, salt);
+
+  return models.User.create({
+    username: req.body.username,
+    password: hash,
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    email: req.body.email,
+    dob: req.body.dob
+  }).then(() => {
+    res.redirect('/');
+  });
+}
